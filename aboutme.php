75c5ccd1656999c51a36f92ccc1e0bcc30a5a6de
rafi-zadanly <32492835+rafi-zadanly@users.noreply.github.com>
2020-04-08 12:02:55 +0700

    Update aboutme.php
    Add phone number and email

--- aboutme.php
+++ aboutme.php
@@ -2,5 +2,6 @@
 echo "Nama : Muhammad Rafi Zadanly";
 echo "Kelas : XI RPL 2";
 echo "Asal Sekolah : SMK Taruna Bhakti";
-echo "Alamat Rumah : Jl. Raya Margonda, Depok";
+echo "Nomor HP : 085894015326";
+echo "Email : rafizadanly@gmail.com";
 ?>
